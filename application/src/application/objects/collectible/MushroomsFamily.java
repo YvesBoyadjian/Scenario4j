9aaaf310831334b88915e8c70150d2de52021c49
Yves BOYAdjian <yboyadjian@hotmail.fr>
2024-02-09 21:56:01 +0100

    Mushrooms

--- application/src/application/objects/collectible/MushroomsFamily.java
+++ application/src/application/objects/collectible/MushroomsFamily.java
@@ -6,16 +6,16 @@ import jscenegraph.coin3d.shaders.inventor.nodes.SoShaderProgram;
 import jscenegraph.database.inventor.SbVec3f;
 import jscenegraph.database.inventor.actions.SoGLRenderAction;
 import jscenegraph.database.inventor.fields.SoMFVec3f;
-import jscenegraph.database.inventor.nodes.SoFile;
-import jscenegraph.database.inventor.nodes.SoLineSet;
-import jscenegraph.database.inventor.nodes.SoNode;
-import jscenegraph.database.inventor.nodes.SoSeparator;
+import jscenegraph.database.inventor.nodes.*;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MushroomsFamily extends ThreeDObjectFamilyBase implements ThreeDObjectFamily {
+
+    private static final float MUSHROOM_SCALE_FACTOR = 0.03f;
 
     SceneGraphIndexedFaceSetShader sg;
 
@@ -30,7 +30,7 @@ public class MushroomsFamily extends ThreeDObjectFamilyBase implements ThreeDObj
     boolean computed;
 
     SoSeparator node = new SoSeparator();
-    private final SoLineSet polylineLineSet = new SoLineSet();
+//    private final SoLineSet polylineLineSet = new SoLineSet();
 
     public String filePath = "ressource/MushroomButton_L2.123c905c8c0c-b164-45b5-ae21-cd1ca6951a92.zip";
 
@@ -58,6 +58,13 @@ public class MushroomsFamily extends ThreeDObjectFamilyBase implements ThreeDObj
             }
         };
 
+        SoScale scale = new SoScale();
+        scale.scaleFactor.setValue(MUSHROOM_SCALE_FACTOR,MUSHROOM_SCALE_FACTOR,MUSHROOM_SCALE_FACTOR);
+
+        node.ref();
+
+        node.addChild(scale);
+
         node.addChild(file);
 
         file.name.setValue(mushroomPath);
@@ -79,36 +86,59 @@ public class MushroomsFamily extends ThreeDObjectFamilyBase implements ThreeDObj
 
         int[] indices = new int[4];
 
+        Random random = new Random(42);
+
         for (int i=0; i<nbPolylinePoints-1; i++) {
             SbVec3f p1 = polylinePoints.get(i);
             SbVec3f p2 = polylinePoints.get(i+1);
-            float pp1Z = sg.getInternalZ(p1.x(),p1.y(),indices,true) + sg.getzTranslation();
-            float pp2Z = sg.getInternalZ(p2.x(),p2.y(),indices,true) + sg.getzTranslation();
 
-            SbVec3f pp1 = new SbVec3f(p1.x(),p1.y(),pp1Z+0.5f);
-            SbVec3f pp2 = new SbVec3f(p2.x(),p2.y(),pp2Z+0.5f);
+            float distance = p2.operator_minus(p1).length();
 
-            polylinePointsOnLand.add(pp1);
-            if (i == nbPolylinePoints-2) {
-                polylinePointsOnLand.add(pp2);
+            float lambda = 1f;
+
+            for (float curviligne=0; curviligne<distance;curviligne++) {
+                float alpha = curviligne/distance;
+                float beta = 1 - alpha;
+
+
+                float dxy = -(1 / lambda) * (float)Math.log( 1 - random.nextDouble() );
+                float angle = (float)Math.PI*2*random.nextFloat();
+                float dx = dxy * (float)Math.sin(angle);
+                float dy = dxy * (float)Math.cos(angle);
+
+                SbVec3f p = p1.operator_mul(alpha).operator_add(p2.operator_mul(beta)).operator_add(new SbVec3f(dx,dy,0));
+                float ppZ = sg.getInternalZ(p.x(),p.y(),indices,true) + sg.getzTranslation();
+                SbVec3f pp = new SbVec3f(p.x(),p.y(),ppZ+0.05f);
+                polylinePointsOnLand.add(pp);
             }
+
+//            float pp1Z = sg.getInternalZ(p1.x(),p1.y(),indices,true) + sg.getzTranslation();
+//            float pp2Z = sg.getInternalZ(p2.x(),p2.y(),indices,true) + sg.getzTranslation();
+//
+//            SbVec3f pp1 = new SbVec3f(p1.x(),p1.y(),pp1Z+0.1f);
+//            SbVec3f pp2 = new SbVec3f(p2.x(),p2.y(),pp2Z+0.1f);
+//
+//            polylinePointsOnLand.add(pp1);
+//            if (i == nbPolylinePoints-2) {
+//                polylinePointsOnLand.add(pp2);
+//            }
         }
 
-        SoVertexProperty vertexProperty = new SoVertexProperty();
-
+//        SoVertexProperty vertexProperty = new SoVertexProperty();
+//
         int polylinePointsSize = polylinePointsOnLand.size();
-
-        for (int i = 0; i < polylinePointsSize; i++) {
-            vertexProperty.vertex.set1Value(i, polylinePointsOnLand.get(i));
-        }
-        vertexProperty.vertex.setNum(polylinePointsSize);
-
-        polylineLineSet.numVertices.set1Value(0, polylinePointsSize);
-        polylineLineSet.vertexProperty.setValue(vertexProperty);
+//
+//        for (int i = 0; i < polylinePointsSize; i++) {
+//            vertexProperty.vertex.set1Value(i, polylinePointsOnLand.get(i));
+//        }
+//        vertexProperty.vertex.setNum(polylinePointsSize);
+//
+//        polylineLineSet.numVertices.set1Value(0, polylinePointsSize);
+//        polylineLineSet.vertexProperty.setValue(vertexProperty);
 
 //        node.addChild(polylineLineSet);
 
-        for (int i=0; i<nbPolylinePoints; i++) {
+        for (int i=0; i<polylinePointsSize; i++) {
             mushroomsCoords.set1Value(i,polylinePointsOnLand.get(i));
             addInstance(i);
         }
@@ -132,6 +162,6 @@ public class MushroomsFamily extends ThreeDObjectFamilyBase implements ThreeDObj
 
     @Override
     public float getViewDistance() {
-        return 99999;
+        return 150;
     }
 }
