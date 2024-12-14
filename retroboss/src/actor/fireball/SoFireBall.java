5229d0c8e6fd5cac9ddce4d0a0ebc84d3c0b8d00
Yves BOYAdjian <yboyadjian@hotmail.fr>
2024-12-14 07:39:14 +0100

    Yellow FireBalls

--- retroboss/src/actor/fireball/SoFireBall.java
+++ retroboss/src/actor/fireball/SoFireBall.java
@@ -3,20 +3,23 @@ package actor.fireball;
 import application.actor.Actor;
 import application.scenegraph.SceneGraphIndexedFaceSetShader;
 import jscenegraph.database.inventor.SbVec3f;
-import jscenegraph.database.inventor.nodes.SoNode;
-import jscenegraph.database.inventor.nodes.SoSeparator;
-import jscenegraph.database.inventor.nodes.SoSphere;
-import jscenegraph.database.inventor.nodes.SoTranslation;
+import jscenegraph.database.inventor.nodes.*;
 
 public class SoFireBall implements Actor {
 
     SoSeparator root = new SoSeparator();
     SoTranslation position = new SoTranslation();
     SbVec3f speed;
+    SoMaterial fireBallColor = new SoMaterial();
     SoSphere fireBall = new SoSphere();
 
     public SoFireBall(SbVec3f speed, SbVec3f initialPosition) {
         root.addChild(position);
+        fireBallColor.diffuseColor.setValue(0,0,0);
+        fireBallColor.ambientColor.setValue(0,0,0);
+        fireBallColor.specularColor.setValue(0,0,0);
+        fireBallColor.emissiveColor.setValue(1,1,0);
+        root.addChild(fireBallColor);
         root.addChild(fireBall);
         fireBall.radius.setValue(0.2f);
         setPosition(initialPosition);
